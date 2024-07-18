3b48e206026cbdc725c4a973162ad62dcf1a2694
Santiago Sanchez <ssanchez@kuenta.co>
2024-07-17 22:00:23 -0500

    add face mesh

---
+++ emotion_processor/main.py
@@ -0,0 +1,11 @@
+import numpy as np
+from emotion_processor.face_mesh.mediapipe import FaceMeshMediapipe
+
+
+class EmotionRecognitionSystem:
+    def __init__(self):
+        self.face_mesh = FaceMeshMediapipe()
+
+    def video_stream_processing(self, face_image: np.ndarray):
+        eye_brows_points, eyes_points, nose_points, mouth_points, control_process, original_image = (
+            self.face_mesh.main_process(face_image))
