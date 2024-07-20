14890fc2d5f7654c9627a129496c384b968c8c79
Santiago Sanchez <ssanchez@kuenta.co>
2024-07-19 20:48:53 -0500

    refactor face mesh

--- emotion_processor/main.py
+++ emotion_processor/main.py
@@ -8,4 +8,5 @@ class EmotionRecognitionSystem:
 
     def video_stream_processing(self, face_image: np.ndarray):
         eye_brows_points, eyes_points, nose_points, mouth_points, control_process, original_image = (
-            self.face_mesh.main_process(face_image, draw=False))
+            self.face_mesh.main_process(face_image, draw=True))
+        print(f'\ncejas: {eye_brows_points} \nojos: {eyes_points} \nnariz: {nose_points} \nboca: {mouth_points}')
